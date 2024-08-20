3bece19babe34e01c81f578da3762ebd8bafb3ed
Wey Gu <weyl.gu@gmail.com>
2024-08-20 18:45:14 +0800

    feat: add query reader

--- ng_nx/query_reader.py
+++ ng_nx/query_reader.py
@@ -1,10 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2023 The NebulaGraph Authors. All rights reserved.
+# Copyright 2024 The NebulaGraph Authors. All rights reserved.
 
 import networkx as nx
 import pandas as pd
 from nebula3.Config import Config
 from nebula3.gclient.net import ConnectionPool
+from nebula3.data.ResultSet import ResultSet
 
 from ng_nx.utils import NebulaGraphConfig, result_to_df
 
@@ -70,6 +71,57 @@ class NebulaReader:
                 )
                 g = nx.compose(g, _g)
             return g
+
+    def release(self):
+        self.connection_pool.close()
+
+    def __del__(self):
+        self.release()
+
+class NebulaQueryReader:
+    def __init__(self, nebula_config: NebulaGraphConfig):
+        self.config = nebula_config
+        self.connection_pool = ConnectionPool()
+        graphd_hosts = nebula_config.graphd_hosts.split(",")
+        graphd_host_list = [
+            (host.split(":")[0], int(host.split(":")[1])) for host in graphd_hosts
+        ]
+        config = Config()
+        assert self.connection_pool.init(
+            graphd_host_list, config
+        ), "Init Connection Pool Failed"
+
+    def read(self, query: str) -> nx.MultiDiGraph:
+        with self.connection_pool.session_context(
+            self.config.user, self.config.password
+        ) as session:
+            assert session.execute(
+                f"USE {self.config.space}"
+            ).is_succeeded(), f"Failed to use space {self.config.space}"
+            
+            result: ResultSet = session.execute(query)
+            assert result.is_succeeded(), f"Query execution failed: {result.error_msg()}"
+            
+            vis_data = result.dict_for_vis()
+            return self._construct_graph(vis_data)
+
+    def _construct_graph(self, vis_data: dict) -> nx.MultiDiGraph:
+        g = nx.MultiDiGraph()
+        
+        # Add nodes
+        for node_data in vis_data['nodes']:
+            g.add_node(node_data['id'], **node_data['props'], labels=node_data['labels'])
+        
+        # Add edges
+        for edge_data in vis_data['edges']:
+            g.add_edge(
+                edge_data['src'],
+                edge_data['dst'],
+                key=edge_data['name'],
+                **edge_data['props']
+            )
+        
+        return g
 
     def release(self):
         self.connection_pool.close()
